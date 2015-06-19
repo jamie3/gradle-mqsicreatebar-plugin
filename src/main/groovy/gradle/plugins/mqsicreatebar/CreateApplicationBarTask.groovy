8b8ac670e4b16a6c9c7d9a73d5cd4b5322e80ea2
Jamie Archibald <jarchibald@westjet.com>
2015-06-19 07:12:05 -0600

    fixed bug which was not including application in the build

--- src/main/groovy/gradle/plugins/mqsicreatebar/CreateApplicationBarTask.groovy
+++ src/main/groovy/gradle/plugins/mqsicreatebar/CreateApplicationBarTask.groovy
@@ -16,11 +16,15 @@ class CreateApplicationBarTask extends DefaultTask {
 		
 		def barFileName = "build/" + project.name.replaceAll(" ", "_") + "-" + project.version + ".bar"
 			
+		// TODO check the .project file
+		// if the project is an Application then
+		// if it is a regular integration project then user needs to define what flows should be included
+		
 		if (new File(barFileName).exists() == false) {
 		
 			println "Creating $barFileName"
 		
-			def cmd = "mqsicreatebar -data ../ -b $barFileName -d \"${project.name}\" -version ${project.version} -cleanBuild -deployAsSource"
+			def cmd = "mqsicreatebar -data ../ -b $barFileName -a \"${project.name}\" -version ${project.version} -cleanBuild -deployAsSource"
 			println cmd
 			
 			def process = cmd.execute()
@@ -28,6 +32,12 @@ class CreateApplicationBarTask extends DefaultTask {
 			if (process.exitValue() != 0) {
 				throw new Exception("mqsicreatebar failed. Error code " + process.exitValue())
 			}
+			
+		} else {
+			// bar file already created, skip.
+			// user must perform a clean
+		
+			// com.ibm.etools.msgbroker.tooling.applicationNature
 		}
 	}
 }
